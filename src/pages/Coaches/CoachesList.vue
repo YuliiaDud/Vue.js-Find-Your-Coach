0d5d1e20c99dd10454b7818d34c07a300be1c74a
juliadud <yuliiadudkina@gmail.com>
2021-11-09 15:08:27 +0200

    displaying coaches info

--- src/pages/Coaches/CoachesList.vue
+++ src/pages/Coaches/CoachesList.vue
@@ -7,17 +7,47 @@
       <button>Refresh</button>
       <router-link to="/register">Register</router-link>
     </div>
-    <ul> LIST OF COACHES</ul>
+    <ul v-if="hasCoaches">
+      <coach-item v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+      ></coach-item>
+    </ul>
+    <h3 v-else>No coaches found</h3>
 
   </section>
 </template>
 
 <script>
+import CoachItem from "../../components/coaches/CoachItem";
 export default {
-  name: "CoachesList"
+  name: "CoachesList",
+  components:{
+    CoachItem
+  },
+  computed:{
+    filteredCoaches(){
+      return this.$store.getters['coaches/coaches']
+    },
+    hasCoaches(){
+      return this.$store.getters['coaches/hasCoaches']
+    }
+  }
 }
 </script>
 
 <style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
 
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
 </style>
