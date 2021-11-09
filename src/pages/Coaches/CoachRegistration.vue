1d003d96c033029f03673743bdc11233a759b101
juliadud <yuliiadudkina@gmail.com>
2021-11-09 14:30:32 +0200

    adding dummy data to components

--- src/pages/Coaches/CoachRegistration.vue
+++ src/pages/Coaches/CoachRegistration.vue
@@ -1,5 +1,5 @@
 <template>
-
+  <div>REgisyer</div>
 </template>
 
 <script>
--- src/pages/Coaches/CoachesList.vue
+++ src/pages/Coaches/CoachesList.vue
@@ -1,5 +1,15 @@
 <template>
+  <section>
+    Filter
+  </section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register</router-link>
+    </div>
+    <ul> LIST OF COACHES</ul>
 
+  </section>
 </template>
 
 <script>
