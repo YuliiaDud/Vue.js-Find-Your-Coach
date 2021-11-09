f23e9ffa84a0298244f12415b00035fa37f930f6
juliadud <yuliiadudkina@gmail.com>
2021-11-09 16:21:54 +0200

    adding filter logic

---
+++ src/components/coaches/CoachFilter.vue
@@ -0,0 +1,70 @@
+<template>
+  <base-card>
+    <h2>Find your Coach</h2>
+    <span class="filter-option">
+      <input type="checkbox" id="frontend" checked @change="setFilter">
+      <label for="frontend">Frontend</label>
+    </span>
+    <span class="filter-option">
+      <input type="checkbox" id="backend" checked @change="setFilter">
+      <label for="backend">Backend</label>
+    </span>
+    <span class="filter-option">
+      <input type="checkbox" id="career" checked @change="setFilter">
+      <label for="career">Career</label>
+    </span>
+  </base-card>
+</template>
+
+<script>
+import BaseCard from "../ui/BaseCard";
+export default {
+  name: "CoachFilter",
+  emits:['change-filter'],
+  components: {BaseCard},
+  data(){
+    return{
+      filters:{
+        frontend: true,
+        backend: true,
+        career: true,
+      }
+    }
+  },
+  methods:{
+    setFilter(event){
+      const inputId = event.target.id; // frontend/backend/career
+      const isChecked = event.target.checked; // true/false
+      const updatedFilters = {
+        ...this.filters, // add filters to updatedfilters
+        [inputId]:isChecked // overwrite inputId with false
+      };
+      this.filters = updatedFilters;
+      this.$emit('change-filter', updatedFilters);
+    }
+  }
+}
+</script>
+
+<style scoped>
+h2 {
+  margin: 0.5rem 0;
+}
+
+.filter-option {
+  margin-right: 1rem;
+}
+
+.filter-option label,
+.filter-option input {
+  vertical-align: middle;
+}
+
+.filter-option label {
+  margin-left: 0.25rem;
+}
+
+.filter-option.active label {
+  font-weight: bold;
+}
+</style>
