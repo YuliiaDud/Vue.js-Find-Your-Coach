892276569de3cd2f01309bd80df37c3b6971f7d9
juliadud <yuliiadudkina@gmail.com>
2021-11-09 15:11:18 +0200

    adding and importing base-card

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from "./store";
-
+import BaseCard from "./components/ui/BaseCard";
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
-
+app.component('base-card', BaseCard);
 app.mount('#app')
