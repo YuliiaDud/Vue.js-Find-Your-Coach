84cff752adfb44afb1d6fa5dbdff9b6b07bbb2bb
juliadud <yuliiadudkina@gmail.com>
2021-11-09 13:56:29 +0200

    importing components to rounter.js

--- src/router.js
+++ src/router.js
@@ -1,16 +1,22 @@
 import {createRouter,createWebHistory} from 'vue-router';
+import CoachesList from "./pages/Coaches/CoachesList";
+import CoachDetails from "./pages/Coaches/CoachDetails";
+import CoachRegistration from "./pages/Coaches/CoachRegistration";
+import ContactCoach from "./pages/Requests/ContactCoach";
+import RequestsReceived from "./pages/Requests/RequestsReceived";
+import NotFound from "./pages/NotFound";
 
 const router = createRouter({
-    history:createWebHistory,
+    history:createWebHistory(),
     routes:[
         {path: '/', redirect: '/coaches'},
-        {path: '/coaches', component: null},
-        {path: '/coaches/:id', component: null,children:[
-                {path: 'contact', component: null}
+        {path: '/coaches', component: CoachesList},
+        {path: '/coaches/:id', component: CoachDetails,children:[
+                {path: 'contact', component: ContactCoach}
             ]},
-        {path: '/register', component: null},
-        {path: '/requests', component: null},
-        {path: '/:notFound(.*)', component: null},
+        {path: '/register', component: CoachRegistration},
+        {path: '/requests', component: RequestsReceived},
+        {path: '/:notFound(.*)', component: NotFound},
     ]
 })
 
