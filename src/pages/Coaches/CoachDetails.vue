a14b60a2ce4b1ca93b3ad159cff296deebed5e5c
juliadud <yuliiadudkina@gmail.com>
2021-11-09 15:43:12 +0200

    filling coach details component

--- src/pages/Coaches/CoachDetails.vue
+++ src/pages/Coaches/CoachDetails.vue
@@ -1,12 +1,35 @@
 <template>
-  <div>+DETAILS</div>
-  <router-view></router-view>
-  <router-link to="/coaches/c1/contact">Contact</router-link>
+  <section>
+    <base-card>
+      <h2>{{ fullName }}</h2>
+      <h3>${{ rate }}/hour</h3>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <header>
+        <h2>Interested? Reach out now!</h2>
+        <base-button link :to="contactLink"></base-button>
+      </header>
+      <router-view></router-view>
+    </base-card>
+  </section>
+  <section>
+    <base-card>
+      <base-badge v-for="area in areas" :key="area" :type="area" :title="area"></base-badge>
+      <p> {{ description }}</p>
+    </base-card>
+  </section>
 </template>
 
 <script>
+import BaseCard from "../../components/ui/BaseCard";
+import BaseButton from "../../components/ui/BaseButton";
+import BaseBadge from "../../components/ui/BaseBadge";
 export default {
-  name: "CoachDetails"
+  name: "CoachDetails",
+  components: {BaseBadge, BaseButton, BaseCard},
+  props:['id']
 }
 </script>
 
