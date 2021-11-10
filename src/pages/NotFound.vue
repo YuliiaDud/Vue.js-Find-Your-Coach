d28c40d2912f61e5adce0be7062a057016387936
juliadud <yuliiadudkina@gmail.com>
2021-11-10 15:16:10 +0200

    adding not found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,10 +1,16 @@
 <template>
-
+  <section>
+    <base-card>
+      <h2>Page Not Found</h2>
+    </base-card>
+  </section>
 </template>
 
 <script>
+import BaseCard from "../components/ui/BaseCard";
 export default {
-  name: "NotFound"
+  name: "NotFound",
+  components: {BaseCard}
 }
 </script>
 
