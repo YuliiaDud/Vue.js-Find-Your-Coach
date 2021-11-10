b1901db9a651a5ae8a4ee068c78ba71f05bb0a11
juliadud <yuliiadudkina@gmail.com>
2021-11-10 14:25:32 +0200

    adding http requests

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,13 +1,58 @@
 export default {
-    addCoach(context, data) {
+    async addCoach(context, data) {
+        const userId = context.rootGetters.userId;
         const coachData = {
-            id: context.rootGetters.userId,
             firstName: data.first,
             lastName: data.last,
             description: data.desc,
             hourlyRate: data.rate,
-            areas: data.area,
+            areas: data.areas,
+        };
+
+        const response = await fetch(
+            `https://main-project-6aa6d-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+            {
+                method: 'PUT',
+                body: JSON.stringify(coachData)
+            }
+        );
+
+        // const responseData = await response.json();
+
+        if (!response.ok) {
+            // error ...
         }
-        context.commit('addCoach', coachData);
+
+        context.commit('addCoach', {
+            ...coachData,
+            id: userId
+        });
+    },
+    async loadCoaches(context) {
+        const response = await fetch(
+            `https://main-project-6aa6d-default-rtdb.firebaseio.com/coaches.json`
+        );
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            // ...
+        }
+
+        const coaches = [];
+
+        for (const key in responseData) {
+            const coach = {
+                id: key,
+                firstName: responseData[key].firstName,
+                lastName: responseData[key].lastName,
+                description: responseData[key].description,
+                hourlyRate: responseData[key].hourlyRate,
+                areas: responseData[key].areas
+            };
+            coaches.push(coach);
+        }
+        console.log(coaches);
+
+        context.commit('setCoaches', coaches);
     }
-}
+};
--- src/store/modules/coaches/mutations.js
+++ src/store/modules/coaches/mutations.js
@@ -1,5 +1,8 @@
 export default {
     addCoach(state, paylod){
         state.coaches.push(paylod);
+    },
+    setCoaches(state, payload) {
+        state.coaches = payload;
     }
 }
