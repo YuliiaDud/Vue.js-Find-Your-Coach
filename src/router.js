2c752fc9852029d993ed2d4839999ca053e14638
juliadud <yuliiadudkina@gmail.com>
2021-11-10 18:04:02 +0200

    adding guard to watch for manually written links

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ import ContactCoach from "./pages/Requests/ContactCoach";
 import RequestsReceived from "./pages/Requests/RequestsReceived";
 import UserAuth from "./pages/auth/UserAuth";
 import NotFound from "./pages/NotFound";
+import store from "./store";
 
 const router = createRouter({
     history:createWebHistory(),
@@ -19,11 +20,19 @@ const router = createRouter({
             children:[
                 {path: 'contact', component: ContactCoach}
             ]},
-        {path: '/register', component: CoachRegistration},
-        {path: '/requests', component: RequestsReceived},
-        {path: '/auth', component: UserAuth},
+        {path: '/register', component: CoachRegistration, meta:{ requiresAuth: true} },
+        {path: '/requests', component: RequestsReceived, meta:{ requiresAuth: true}},
+        {path: '/auth', component: UserAuth, meta:{ requiresUnauth: true}},
         {path: '/:notFound(.*)', component: NotFound},
     ]
 })
-
+router.beforeEach(function (to, from,next){
+    if(to.meta.requiresAuth && !store.getters.isAuthenticated){
+        next('auth');
+    } else if(to.meta.requiresUnauth && store.getters.isAuthenticated){
+        next('/coaches');
+    }else{
+        next();
+    }
+})
 export default router;
